dd3e321227472beba52f0b65b22de542fdac6345
Nicolas Corrarello <nicolas@corrarello.com>
2017-05-02 21:54:15 +0100

    Creating a real base class

--- Puppetfile
+++ Puppetfile
@@ -1,10 +1,27 @@
 forge "http://forge.puppetlabs.com"
 
+
 # Modules from the Puppet Forge
 # Versions should be updated to be the latest at the time you start
-#mod "puppetlabs/inifile",     '1.5.0'
-#mod "puppetlabs/stdlib",      '4.11.0'
-#mod "puppetlabs/concat",      '2.1.0'
+mod "puppetlabs/inifile"
+mod "puppetlabs/stdlib"
+mod "puppetlabs/concat"
+mod "puppetlabs/mysql"
+mod 'puppetlabs-vcsrepo', '1.4.0'
+mod 'puppetlabs-apache', '1.10.0'
+mod 'stahnma-epel', '1.2.2'
+mod 'puppet-staging'
+mod 'KyleAnderson-consul', '3.0.0'
+mod 'puppet-archive'
+mod 'spiette-selinux', '0.5.4'
+mod 'jsok-vault', '1.1.8'
+mod 'camptocamp-openssl', '1.8.2'
+mod 'cristifalcas-subscription_manager', '0.2.0'
+mod 'thrnio-ip', '1.0.0'
+mod 'bodgit-bodgitlib', '1.7.0'
+mod 'bodgit-openldap', '1.4.0'
+mod 'saz-ssh', '3.0.1'
+mod 'dudemcbacon-nomad', '0.0.3'
 
 # Modules from Git
 # Examples: https://github.com/puppetlabs/r10k/blob/master/doc/puppetfile.mkd#examples
--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,48 @@
 class profile::base {
 
-  #the base profile should include component modules that will be on all nodes
+  include ::ntp
+  include ::apt
+
+  package { ['curl','oracle-java8-installer','libapparmor-dev']:
+    ensure  => present,
+    require => Exec['acceptterms'],
+  }
+
+  apt::key { 'OracleJava':
+    id      => 'EEA14886',
+    server  => 'keyserver.ubuntu.com',
+  }
+
+  apt::source { 'puppetlabs':
+    location => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
+    release  => 'trusty',
+    repos    => 'main',
+    key      => {
+      'id'     => 'EEA14886',
+      'server' => 'keyserver.ubuntu.com',
+    },
+    notify  => Exec['acceptterms'],
+  }
+
+  exec { 'acceptterms':
+    command     => '/bin/bash -c "echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections  && echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections"',
+    refreshonly => true,
+  }
+  
+  exec { 'dockerinstall':
+    command => '/usr/bin/curl -sSL get.docker.com | /bin/sh',
+    creates => '/usr/bin/docker',
+  }
+
+  user { 'ncorrare':
+    managehome => true,
+    password   => '$6$wg6hZoXB$Ilj.WHrNHD.4apIzxOeuu7gBeMFG.vhQWOlWM39O87hcS52yH7LINjP8d72eGVUx9bFKkXRW10BH0PObe1/qC.',
+    groups     => ["sudo"],
+    group      => ncorrare,
+  }
+  
+  group { 'ncorrare':
+    ensure => 'present',
+  }
 
 }
